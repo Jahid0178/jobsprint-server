ce059bc6aadbac98295e710c8653aa9defc8d4d3
Jahid0178 <jahidulhaque767@gmail.com>
2024-11-28 20:26:57 +0600

    feat: Add user registration and login functionality with JWT-based authentication
    - Implemented `registerUser` function:
      - Validates input fields and checks for password match.
      - Ensures duplicate user prevention by checking email uniqueness.
      - Hashes passwords using bcrypt before storing them in the database.
      - Returns success message on successful user registration.
    
    - Implemented `loginUser` function:
      - Validates input fields and checks for user existence by email.
      - Verifies password with bcrypt and generates JWT on successful login.
      - Sets a secure, HTTP-only cookie (`jobsprint-auth-token`) for authentication.
    
    - Includes meaningful error messages for common issues like missing fields, duplicate users, and incorrect login credentials.

---
+++ controllers/user.controller.js
@@ -0,0 +1,80 @@
+const User = require("../models/user.model");
+const bcypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+  try {
+    const { fullName, email, password, confirmPassword } = req.body;
+
+    if (!fullName || !email || !password || !confirmPassword) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "passwords do not match" });
+    }
+
+    const existingUser = await User.findOne({ email }).exec();
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcypt.hash(password, 10);
+
+    await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log("register user error", error);
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ email }).exec();
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "User does not exist" });
+    }
+
+    const passwordMatched = await bcypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!passwordMatched) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    const token = jwt.sign(
+      { email: existingUser.email, _id: existingUser._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.cookie("jobsprint-auth-token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+    res.status(200).json({ message: "User logged in successfully", token });
+  } catch (error) {
+    console.log("login user error", error);
+  }
+};
+
+module.exports = { registerUser, loginUser };
