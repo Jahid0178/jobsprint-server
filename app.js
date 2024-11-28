6d3eaf6497ab09b10574bdefa28f6c564895a631
Jahid0178 <jahidulhaque767@gmail.com>
2024-11-28 20:34:41 +0600

    feat: Add user authentication routes for registration and login
    - Created `user.route.js` file using `express.Router` for user-related routes.
    - Added `POST /register` route to handle user registration using `registerUser` controller.
    - Added `POST /login` route to handle user login using `loginUser` controller.
    - Ensured modular design for cleaner route organization and easier maintenance.

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+const userRouter = require("./routes/user.route");
+const authenticatedMiddleware = require("./middlewares/authenticated-middleware");
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", authenticatedMiddleware, (req, res) => {
   res.send("Server is running");
 });
+
+app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("db_connection_success");
+  } catch (error) {
+    console.log("db_connection_error", error);
+  }
+};
+
+connectDB();
